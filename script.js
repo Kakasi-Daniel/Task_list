8dcb14258ef354a168a3a34b19581c91d7c4304e
Kakasi Daniel <daniel_kksi@Kakasis-iMac.local>
2020-06-09 19:02:23 +0300

    workins search

--- script.js
+++ script.js
@@ -5,91 +5,89 @@ const rmvBtn = document.querySelector('.delete_task');
 const clearBtn = document.querySelector('.clear_tasks');
 const taskTitle = document.querySelector('.task_title');
 const searchInput = document.querySelector('.search_input');
-const dTasks = document.querySelectorAll('.task');
-
-
-document.addEventListener('DOMContentLoaded' , e => {
+searchInput.addEventListener('input', (e) => {
+  const txt = e.target.value.toLowerCase();
+  document.querySelectorAll('.task').forEach(function (t) {
+    const item = t.firstElementChild.textContent;
+    if (item.toLowerCase().indexOf(txt) != -1) {
+      t.style.display = 'flex';
+    } else {
+      t.style.display = 'none';
+    }
+  });
+});
+document.addEventListener('DOMContentLoaded', (e) => {
   let tasks;
   if (localStorage.getItem('tasks') === null) {
     tasks = [];
   } else {
     tasks = JSON.parse(localStorage.getItem('tasks'));
   }
-
-  tasks.forEach( e=>{
+  tasks.forEach((e) => {
     let taskL = `<div class="task">
-  <div class="task_name">${e}</div>
-  <div class="delete_task">&times;</div>
-  </div>`;
-  taskList.insertAdjacentHTML('afterbegin', taskL);
-  })
+<div class="task_name">${e}</div>
+<div class="delete_task">&times;</div>
+</div>`;
+    taskList.insertAdjacentHTML('afterbegin', taskL);
+  });
   checkList();
-})
-
-addBtn.addEventListener('click' , () => {
+});
+addBtn.addEventListener('click', () => {
   addTask();
   checkList();
-})
-
-window.addEventListener('keyup', e => {
-  if (e.keyCode === 13){
+});
+window.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
     addTask();
     checkList();
   }
-})
-
-window.addEventListener('click', e => {
-   if (e.target.classList.contains('delete_task')){
-      let tasks;
-      if (localStorage.getItem('tasks') === null) {
-        tasks = [];
-      } else {
-        tasks = JSON.parse(localStorage.getItem('tasks'));
-      }
-      const index = tasks.indexOf(e.target.previousElementSibling.textContent);
-      if (index > -1) {
-        tasks.splice(index, 1);
-      }
-      localStorage.setItem('tasks', JSON.stringify(tasks));
-      e.target.parentElement.remove();
-      checkList();
-}})
-
+});
+window.addEventListener('click', (e) => {
+  if (e.target.classList.contains('delete_task')) {
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+      tasks = [];
+    } else {
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    const index = tasks.indexOf(e.target.previousElementSibling.textContent);
+    if (index > -1) {
+      tasks.splice(index, 1);
+    }
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    e.target.parentElement.remove();
+    checkList();
+  }
+});
 clearBtn.addEventListener('click', () => {
   taskList.innerHTML = '';
   let tasks;
   localStorage.clear();
   checkList();
-})
-
-function addTask(){
+});
+function addTask() {
   const text = taskInput.value;
   const task = `<div class="task">
-  <div class="task_name">${text}</div>
-  <div class="delete_task">&times;</div>
-  </div>`;
+<div class="task_name">${text}</div>
+<div class="delete_task">&times;</div>
+</div>`;
   taskList.insertAdjacentHTML('afterbegin', task);
-  
   let tasks;
   if (localStorage.getItem('tasks') === null) {
     tasks = [];
   } else {
     tasks = JSON.parse(localStorage.getItem('tasks'));
   }
-
   tasks.push(text);
-
   localStorage.setItem('tasks', JSON.stringify(tasks));
-
   taskInput.value = '';
 }
-
-function checkList () {
-  if (taskList.innerHTML === ''){
+function checkList() {
+  if (taskList.innerHTML === '') {
     taskTitle.style.display = 'none';
     clearBtn.style.display = 'none';
     searchInput.style.display = 'none';
-  }else{
+  } else {
     taskTitle.style.display = 'block';
     clearBtn.style.display = 'block';
     searchInput.style.display = 'block';
